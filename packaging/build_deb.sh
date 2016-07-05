087f890b78bee6b25329e5c3a14e983cd4b352fe
Konstantin Kirik <smlcx.mail@gmail.com>
2016-07-05 05:31:35 +0300

    * Issue: #1 Fixed build_deb script, added generated stdeb and setup.py to version control

--- packaging/build_deb.sh
+++ packaging/build_deb.sh
@@ -12,16 +12,16 @@ echo "======================"
 echo "Modifying stdeb.cfg"
 echo "======================"
 
-DEB_VERSION=$( cat ./packaging/dev-version )
+DEB_VERSION=$( cat ./packaging/deb-version )
 LSB_RELEASE=$( lsb_release -r | sed -e "s/Release:\t*//")
-sed -e "s/Debian-Version:/Debian-Version: ${DEV_VERSION}/" ./packaging/stdeb.cfg.in | sed -e "s/suite:/suite: ${LSB_RELEASE}/" > ./stdeb.cfg
+sed -e "s/Debian-Version:/Debian-Version: ${DEB_VERSION}/" ./packaging/stdeb.cfg.in | sed -e "s/suite:/suite: ${LSB_RELEASE}/" > ./stdeb.cfg
 
 echo "======================"
 echo "Building package"
 echo "======================"
 
 rm -rf deb_dist
-python ./setup.py --command-packages=stdeb.command sdist_dsc --suite="testing"
+python ./setup.py --command-packages=stdeb.command sdist_dsc --suite="${LSB_RELEASE}"
 cd ./deb_dist/python-utils-${VSTRING}
 debuild
 cd ../../
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "python-utils",
+    version = "0.1",
+    author = "Konstantin Kirik (snegovick)",
+    author_email = "snegovick@uprojects.org",
+    description = ("Basic utilities for python."),
+    license = "MIT",
+    keywords = "utility, log",
+    url = "http://snegovick.me",
+    packages=['utils'],
+    scripts=[],
+    long_description=read('README.md'),
+    dependency_links = [],
+    install_requires = [],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+    ],
+)
+
