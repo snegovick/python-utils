76345901b7ee84ad9e3b2fe1fe67b2c625f53db8
Konstantin Kirik <smlcx.mail@gmail.com>
2016-07-11 10:25:51 +0300

    * Issue #4 : Fixed lsb_release parser

--- packaging/build_deb.sh
+++ packaging/build_deb.sh
@@ -4,6 +4,7 @@ echo "======================"
 echo "Modifying setup.py"
 echo "======================"
 
+PYPKG_NAME="utils"
 VSTRING=$( cat ./packaging/version )
 echo "VSTRING: ${VSTRING}"
 sed -r -e "s/( *)version =/\1version = \"${VSTRING}\",/" ./packaging/setup.py.in > setup.py
@@ -13,7 +14,7 @@ echo "Modifying stdeb.cfg"
 echo "======================"
 
 DEB_VERSION=$( cat ./packaging/deb-version )
-LSB_RELEASE=$( lsb_release -r | sed -e "s/Release:\t*//")
+LSB_RELEASE=$( lsb_release -a | grep "Codename" | sed -e "s/Codename:\t*//")
 sed -e "s/Debian-Version:/Debian-Version: ${DEB_VERSION}/" ./packaging/stdeb.cfg.in | sed -e "s/suite:/suite: ${LSB_RELEASE}/" > ./stdeb.cfg
 
 echo "======================"
@@ -22,6 +23,6 @@ echo "======================"
 
 rm -rf deb_dist
 python ./setup.py --command-packages=stdeb.command sdist_dsc --suite="${LSB_RELEASE}"
-pushd ./deb_dist/utils-${VSTRING}
+pushd ./deb_dist/${PYPKG_NAME}-${VSTRING}
 debuild
 popd
